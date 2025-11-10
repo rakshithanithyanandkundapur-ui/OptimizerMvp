352059f809e6bb6555bb8bfc8e553647a3b2446d
Karthik V Naik <karthikn97531@Karthiks-MacBook-Air.local>
2025-11-11 00:42:55 +0530

    All the cosmetic UI bug fixes those are of high priority and phase 1 are implemented in this PR

--- vite-project/src/components/CardModal.jsx
+++ vite-project/src/components/CardModal.jsx
@@ -20,7 +20,7 @@ function CardModal({ showModal, onClose }) {
                 {/* Close button */}
                 <button
                     onClick={onClose}
-                    className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+                    className="absolute top-2 right-4 text-gray-500 hover:text-gray-700"
                 >
                     <X size={24} />
                 </button>
